a122aa2bdf495dbb816b857a50fdc122e339be56
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-01-28 16:53:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,17 +23,22 @@ public class Main {
             statement.executeUpdate("drop table if exists documents");
             statement.executeUpdate("drop table if exists authors");
             statement.executeUpdate("drop table if exists keywords");
+            statement.executeUpdate("drop table if exists books");
             statement.executeUpdate("create table if not exists users (id mediumint not null auto_increment primary key, name varchar(255) not null, surname varchar(255) not null, card_number bigint not null, phone_number varchar(255) not null, id_address mediumint not null, documents varchar(255))");
             statement.executeUpdate("create table if not exists addresses(id_address mediumint not null auto_increment primary key, country varchar(1024) not null, town varchar(1024) not null, street varchar(1024), house_number mediumint not null, apartment_number mediumint not null, postcode varchar(1024) not null)");
             statement.executeUpdate("create table if not exists documents (id mediumint not null auto_increment primary key, title varchar(1024) not null, authors varchar(1024), keywords varchar(1024), price mediumint, amount mediumint not null, type varchar(30))");
             statement.executeUpdate("create table if not exists authors (id mediumint not null auto_increment primary key, name varchar(1024) not null, documents varchar(1024))");
             statement.executeUpdate("create table if not exists keywords (id mediumint not null auto_increment primary key, keyword varchar(1024) not null, documents varchar(1024))");
-            statement.executeUpdate("create table if not exists books (id_document mediumint not null primary key, publisher varchar(1024) not null, year mediumint, edition_number mediumint, best_selller bit)");
+            statement.executeUpdate("create table if not exists books (id_document mediumint not null primary key, publisher varchar(1024) not null, year mediumint, edition_number mediumint, best_seller mediumint)");
 
-            Document d = new Document(conn, scan2);
-            d.read();
-            d.save();
+            Book b = new Book(conn, scan2);
+            b.read();
+            b.setBest_seller(true);
+            b.save();
             Librarian l = new Librarian(conn, scan2);
+            l.read();
+            l.save();
+
         }
 
     }
