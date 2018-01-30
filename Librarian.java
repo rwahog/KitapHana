8c13a8fd496648996d79a1673becad6bb14fd26f
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-01-30 22:25:26 +0300

    Update Librarian.java

--- Librarian.java
+++ Librarian.java
@@ -15,8 +15,22 @@ public class Librarian extends User {
 
     }
     //Document
-    public void addDocument(Document document){
+    public void addDocument(Document document) throws SQLException {
+        System.out.println("Type: ");
+        String s = in.next();
+        if(s.equals("Book")){
+            Book book = new Book(connection, in);
+            book.read();
+            book.save();
+        }
+        else if(s.equals("AV")){
+            AVMaterial avMaterial = new AVMaterial(connection, in);
+            avMaterial.read();
+            avMaterial.save();
+        }
+        else if(s.equals("JA")){
 
+        }
     }
     public void  removeDocument(Document document){
 
