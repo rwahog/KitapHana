175dbd732cf3f23528d3f76872edd870e1780ead
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-02-01 10:01:07 +0300

    Add files via upload

---
+++ Patron.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Patron extends User {
+    Patron(Connection connection, Scanner in) throws SQLException {
+        super(connection, in);
+    }
+
+    @Override
+    public void save() throws SQLException {
+        super.save();
+        statement.executeUpdate("update users set type = 'patron' where id = '"+id+"'");
+    }
+
+    @Override
+    public void login() throws SQLException {
+        super.login();
+        ResultSet resultSet = statement.executeQuery("select * from users where id = '"+id+"'");
+        if(resultSet.next()){
+            if(resultSet.getString("type").equals("patron")){
+                System.out.println("Successfully");
+            }
+            else{
+                System.out.println("Wrong card number");
+            }
+        }
+    }
+}
