e8b5a17758cbf420076c6c1c6522e61133423d2d
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-02-01 09:59:57 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,7 +26,7 @@ public class Main {
             statement.executeUpdate("drop table if exists books");
             statement.executeUpdate("drop table if exists av");
             statement.executeUpdate("drop table if exists ja");
-            statement.executeUpdate("create table if not exists users (id mediumint not null auto_increment primary key, name varchar(255) not null, surname varchar(255) not null, card_number bigint not null, phone_number varchar(255) not null, id_address mediumint not null, documents varchar(255))");
+            statement.executeUpdate("create table if not exists users (id mediumint not null auto_increment primary key, name varchar(1024) not null, surname varchar(1024) not null, card_number bigint not null, phone_number varchar(1024) not null, id_address mediumint not null, documents varchar(1024), type varchar(1024))");
             statement.executeUpdate("create table if not exists addresses(id_address mediumint not null auto_increment primary key, country varchar(1024) not null, town varchar(1024) not null, street varchar(1024), house_number mediumint not null, apartment_number mediumint not null, postcode varchar(1024) not null)");
             statement.executeUpdate("create table if not exists documents (id mediumint not null auto_increment primary key, title varchar(1024) not null, authors varchar(1024), keywords varchar(1024), price mediumint, amount mediumint not null, type varchar(30))");
             statement.executeUpdate("create table if not exists authors (id mediumint not null auto_increment primary key, name varchar(1024) not null, documents varchar(1024))");
