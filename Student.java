44c184dfd2f4b0692e64c294e88acff289c78d96
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-02-04 19:05:45 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,4 +6,10 @@ public class Student extends User{
     Student(Connection connection, Scanner in) throws SQLException {
         super(connection, in);
     }
+
+    @Override
+    public void save() throws SQLException {
+        super.save();
+        statement.executeUpdate("update users set type = 'student' where id = '"+id+"'");
+    }
 }
