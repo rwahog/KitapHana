44f1b6d63b5e7768286e7556ec53a3f8a990678d
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-02-04 13:17:25 +0300

    Update JournalArticle.java

--- JournalArticle.java
+++ JournalArticle.java
@@ -1,92 +1,80 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 import java.util.Scanner;
 
-public class JournalArticle extends Document{
-    protected String journal_name, date;
-    protected ArrayList<String> editors;
-
-    JournalArticle(Connection connection, Scanner in) throws SQLException {
-        super(connection, in);
-        editors = new ArrayList<String>();
+public class Keyword {
+    private int id;
+    private ArrayList<Document> documents;
+    private String keyword;
+    private Statement statement;
+    private Connection connection;
+    private Scanner in;
+    Keyword(Connection connection, Scanner in) throws SQLException {
+        this.connection = connection;
+        this.in = in;
+        statement = connection.createStatement();
+        documents = new ArrayList<Document>();
     }
-
-    @Override
-    public void read() throws SQLException {
-        super.read();
-        System.out.println("Journal name: ");
-        setJournal_name(in.next());
-        System.out.println("Date: ");
-        setDate(in.next());
-        System.out.println("Editors");
-        while(in.hasNext()){
-            String editor = in.next();
-            if(editor.equals("end")) break;
-            addEditor(editor);
-        }
-    }
-
-    @Override
     public void save() throws SQLException {
-        super.save();
-        statement.executeUpdate("update documents set type = 'ja' where id = 'id'");
-        ResultSet resultSet = statement.executeQuery("select * from ja where id_document = '"+id+"'");
+        ResultSet resultSet = statement.executeQuery("select * from keywords where keyword = '"+keyword+"'");
         if(resultSet.next()){
-            statement.executeUpdate("update ja set journal_name = '"+journal_name+"', date = '"+date+"', editors = '"+getEditorsAsString()+"' where id_document = '"+id+"'");
+
         }
-        else{
-            statement.executeUpdate("insert into ja (id_document, journal_name, date, editors) values('"+id+"', '"+journal_name+"', '"+date+"', '"+getEditorsAsString()+"')");
+        else {
+            statement.executeUpdate("insert into keywords (keyword, documents) values ('" + keyword + "', '" + getDocumentsAsString() + "')");
+            resultSet = statement.executeQuery("select * from keywords where keyword = '" + keyword + "'");
+            if (resultSet.next()) {
+                id = resultSet.getInt("id");
+            }
         }
+    }
+    public void setVariablesKnowingKeyword() throws SQLException {
+        ResultSet resultSet = statement.executeQuery("select * from keywords where name = '"+keyword+"'");
+        if(resultSet.next()){
+            id = resultSet.getInt("id");
+            addDocumentsFromString(resultSet.getString("documents"));
+        }
+    }
+    //Keyword
+    public String getKeyword() {
+        return keyword;
     }
 
-    //Journal_name
-    public void setJournal_name(String journal_name) {
-        this.journal_name = journal_name;
+    public void setKeyword(String keyword) throws SQLException {
+        this.keyword = keyword;
     }
-    public String getJournal_name() {
-        return journal_name;
+    //Documents
+
+    public ArrayList<Document> getDocuments() {
+        return documents;
     }
-    //Date
-    public void setDate(String date) {
-        this.date = date;
+    public void addDocument(Document document) throws SQLException {
+        documents.add(document);
     }
-    public String getDate() {
-        return date;
-    }
-    //Editors
-    public void setEditors(ArrayList<String> editors) {
-        this.editors = editors;
-    }
-    public ArrayList<String> getEditors() {
-        return editors;
-    }
-    public String getEditorsAsString(){
-        String s = "";
-        for(int i = 0; i<editors.size(); i++){
-            if(i<editors.size()-1) s = s.concat(editors.get(i) + ", ");
-            else s = s.concat(editors.get(i));
-        }
-        return s;
-    }
-    public void setEditorsFromString(String s){
+    public void addDocumentsFromString(String s) throws SQLException {
         for(int i = 0; i<s.length(); i++){
             String cur = "";
             int j = i;
             while(j<s.length() && s.charAt(j) != ','){
-                cur = cur.concat(String.valueOf(s.charAt(j)));
+                cur.concat(String.valueOf(s.charAt(j)));
                 j++;
             }
-            i = j+1;
-            addEditor(cur);
+            Document document = new Document(connection, in);
+            addDocument(document);
         }
     }
-    public void addEditor(String editor){
-        editors.add(editor);
+    public void removeDocument(Document document) throws SQLException {
+        documents.remove(document);
     }
-    public void removeEditor(String editor){
-        editors.add(editor);
+    public String getDocumentsAsString(){
+        String s = "";
+        for(int i = 0; i<documents.size(); i++){
+            if(i<documents.size()-1) s = s.concat(documents.get(i).getTitle() + ", ");
+            else s = s.concat(documents.get(i).getTitle());
+        }
+        return s;
     }
 }
