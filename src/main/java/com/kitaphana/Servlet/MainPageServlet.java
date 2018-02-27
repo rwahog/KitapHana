d05c6d14470f3a4fd770adfbaf3e70a1e08b79b0
rwahog <rwahog@gmail.com>
2018-02-27 21:48:21 +0300

    Update MainPageServlet.java

--- src/main/java/com/kitaphana/Servlet/MainPageServlet.java
+++ src/main/java/com/kitaphana/Servlet/MainPageServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns="/mainpage")
+@WebServlet(urlPatterns="/main")
 public class MainPageServlet extends HttpServlet {
 
     @Override
