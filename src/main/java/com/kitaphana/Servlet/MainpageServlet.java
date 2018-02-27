a294025b50dfa951f2bbfb83515c69817af83cf2
AygulMalikova <amalikova19@gmail.com>
2018-02-27 21:31:32 +0300

    Services And Servlets

--- src/main/java/com/kitaphana/Servlet/MainpageServlet.java
+++ src/main/java/com/kitaphana/Servlet/MainpageServlet.java
@@ -1,17 +1,16 @@
 package com.kitaphana.Servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/mainpage")
-public class MainpageServlet extends HttpServlet {
+public class MainPageServlet extends HttpServlet {
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         request.getRequestDispatcher("/WEB-INF/views/mainPage.jsp").forward(request, response);
     }
-
 }
