b6146b008630dced897b39c6425e82e974459241
GeorgyKhorushevsky <35738786+GeorgyKhorushevsky@users.noreply.github.com>
2018-03-02 02:14:30 +0300

    test

---
+++ src/main/java/com/kitaphana/Service/AVMaterial.java
@@ -0,0 +1,10 @@
+package com.kitaphana.Service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AVMaterial extends Document {
+
+}
