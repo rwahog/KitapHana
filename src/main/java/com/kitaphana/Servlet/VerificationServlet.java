80c7da40d99462b75cb10048cadde63f5a5bcec3
rwahog <arcticboyinblue@gmail.com>
2018-03-01 23:51:28 +0300

    update verification page

---
+++ src/main/java/com/kitaphana/Servlet/VerificationServlet.java
@@ -0,0 +1,17 @@
+package com.kitaphana.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/verification")
+public class VerificationServlet extends HttpServlet{
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/views/verification.jsp").forward(request, response);
+    }
+}
