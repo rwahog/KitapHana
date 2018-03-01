0ff2700a2e692464c1cf2316758e281ecd87892c
rwahog <rwahog@gmail.com>
2018-03-02 00:15:50 +0300

    Update VerificationServlet.java

--- src/main/java/com/kitaphana/Servlet/VerificationServlet.java
+++ src/main/java/com/kitaphana/Servlet/VerificationServlet.java
@@ -8,10 +8,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/verification")
-public class VerificationServlet extends HttpServlet{
+public class VerificationServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getRequestDispatcher("/WEB-INF/views/verification.jsp").forward(request, response);
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getSession().setAttribute("name", request.getParameter("name"));
+        request.getSession().setAttribute("surname", request.getParameter("surname"));
+        response.sendRedirect("/main");
     }
 }
