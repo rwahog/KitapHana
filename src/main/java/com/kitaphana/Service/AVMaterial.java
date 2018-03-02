6e86360c6fda6c31def48adc58450d7f6914464e
rwahog <rwahog@gmail.com>
2018-03-02 06:56:12 +0300

    Create AVMaterial.java

---
+++ src/main/java/com/kitaphana/Service/AVMaterial.java
@@ -0,0 +1,27 @@
+package com.kitaphana.Service;
+
+public class AVMaterial extends Document {
+
+    protected String title;
+    protected int id;
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
