1a19146dc774939fa4f60a7dc35dbba291db2c48
rwahog <rwahog@gmail.com>
2018-03-03 04:10:49 +0300

    Update and rename src/main/java/com/kitaphana/Service/User.java to src/main/java/com/kitaphana/Entities/User.java

--- src/main/java/com/kitaphana/Entities/User.java
+++ src/main/java/com/kitaphana/Entities/User.java
@@ -1,12 +1,9 @@
-package com.kitaphana.Service;
-
-import com.kitaphana.algorithm.Address;
-import com.kitaphana.algorithm.Document;
+package com.kitaphana.Entities;
 
 import java.util.ArrayList;
 
 public class User {
-    protected String phone_number, name, surname, password, possible_type;
+    protected String phone_number, name, surname, password, possible_type, email;
     protected Address address;
     protected long card_number, maxdays, day = 24 * 60 * 60 * 1000;
     protected int id;
@@ -32,6 +29,22 @@ public class User {
     public void setPossible_type(String possible_type) {
         this.possible_type = possible_type;
     }
+    
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     public int getId() {
         return id;
@@ -51,5 +64,21 @@ public class User {
 
     public String getPossible_type() {
         return possible_type;
+    }
+    
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 }
