4facee438dfbaff9bdbcc43645a191d0e690c0ea
rwahog <rwahog@gmail.com>
2018-03-03 04:14:46 +0300

    Update AVMaterial.java

--- src/main/java/com/kitaphana/Entities/AVMaterial.java
+++ src/main/java/com/kitaphana/Entities/AVMaterial.java
@@ -1,4 +1,4 @@
-package com.kitaphana.Service;
+package com.kitaphana.Entities;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -6,5 +6,15 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AVMaterial extends Document {
+    protected String title;
+    
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    @Override
+    public String getTitle() {
+        return title;
+    }
 }
