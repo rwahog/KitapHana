aea72c9d98fbeda8d408e7ff9ff029cf15246941
rwahog <rwahog@gmail.com>
2018-03-03 04:15:07 +0300

    Update AVMaterial.java

--- src/main/java/com/kitaphana/Entities/AVMaterial.java
+++ src/main/java/com/kitaphana/Entities/AVMaterial.java
@@ -1,10 +1,5 @@
 package com.kitaphana.Entities;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
-
 public class AVMaterial extends Document {
     protected String title;
     
