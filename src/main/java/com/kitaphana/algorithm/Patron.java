091957fa0eb7bb817b84fe98e89f81471e5dd35b
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-03-05 20:59:09 +0300

    Update Patron.java

--- src/main/java/com/kitaphana/algorithm/Patron.java
+++ src/main/java/com/kitaphana/algorithm/Patron.java
@@ -1,7 +1,7 @@
-package com.kitaphana.algorithm;
-
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Patron extends User {
