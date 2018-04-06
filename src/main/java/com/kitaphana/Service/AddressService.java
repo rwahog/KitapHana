c89200b42ea246146952d00c4938b6bfa3d61de8
rwahog <rwahog@gmail.com>
2018-04-06 18:41:03 +0300

    deleted unnecessary methods

--- src/main/java/com/kitaphana/Service/AddressService.java
+++ src/main/java/com/kitaphana/Service/AddressService.java
@@ -29,14 +29,4 @@ public class AddressService {
     public ArrayList<Address> findAllAddresses() throws SQLException {
         return addressDAO.findAll();
     }
-
-    public Address createAddress(String country, String town, String street,
-                                 int house_number, int apartment_number, String postcode, long id_address) {
-        return new Address(country, town, street, house_number, apartment_number, postcode, id_address);
-    }
-
-    public Address createAddressWithoutId(String country, String town, String street,
-                                          int house_number, int apartment_number, String postcode) {
-        return new Address(country, town, street, house_number, apartment_number, postcode);
-    }
 }
