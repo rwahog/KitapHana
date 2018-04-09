df4cc6367ed6ac1e4ad1061635bac0eec220144f
rwahog <rwahog@gmail.com>
2018-04-09 11:14:16 +0300

    Update bookDAOImpl.java

--- src/main/java/com/kitaphana/dao/bookDAOImpl.java
+++ src/main/java/com/kitaphana/dao/bookDAOImpl.java
@@ -12,15 +12,14 @@ import java.util.List;
 public class bookDAOImpl implements bookDAO {
 
     Database db = Database.getInstance();
-
+    commonDAOImpl commonDAO = new commonDAOImpl();
     //Prepared statements which will be used in methods below.
     private static final String FIND_ALL = "SELECT * FROM books";
     private static final String FIND_BY_ID = "SELECT * FROM books WHERE id=?";
     private static final String UPDATE = "UPDATE books SET title=?, publisher=?, year=?, edition_number=?, best_seller=? WHERE id=?";
-    private static final String DELETE = "DELETE FROM books WHERE id=?";
+    private static final String UPDATE_BY_ID_DOCUMENT = "UPDATE books SET title=?, publisher=?, year=?, edition_number=?, best_seller=? WHERE document_id=?";
     private static final String FIND_BY_ID_DOCUMENT = "SELECT * FROM books WHERE document_id=?";
     private static final String INSERT = "INSERT INTO books (title, publisher, year, edition_number, best_seller, document_id) VALUES (?,?,?,?,?,?)";
-    private static final String DELETE_BY_ID_DOCUMENT = "DELETE FROM books WHERE document_id=?";
 
     /**
      * Search database table "books" to find information about book
@@ -28,9 +27,8 @@ public class bookDAOImpl implements bookDAO {
      * "documents". Return null if the object is not present.
      * @param id - document id (SQL table column)
      * @return - book object
-     * @throws SQLException
      */
-    public Book findByIdDocument(long id) throws SQLException {
+    public Book findByIdDocument(long id) {
         Book book = null;
         try {
             PreparedStatement ps = db.con.prepareStatement(FIND_BY_ID_DOCUMENT);
@@ -42,8 +40,8 @@ public class bookDAOImpl implements bookDAO {
                 book.setTitle(rs.getString("title"));
                 book.setPublisher(rs.getString("publisher"));
                 book.setYear(rs.getInt("year"));
-                book.setEdition_number(rs.getInt("edition_number"));
-                book.setBest_seller(rs.getInt("best_seller"));
+                book.setEditionNumber(rs.getInt("edition_number"));
+                book.setBestseller(rs.getInt("best_seller"));
             }
 
             rs.close();
@@ -62,10 +60,9 @@ public class bookDAOImpl implements bookDAO {
      * present.
      * @param id - book id (column)
      * @return - book object
-     * @throws SQLException
      */
     @Override
-    public Book findById(long id) throws SQLException {
+    public Book findById(long id) {
         Book book = null;
 
         try {
@@ -78,8 +75,8 @@ public class bookDAOImpl implements bookDAO {
                 book.setTitle(rs.getString("title"));
                 book.setPublisher(rs.getString("publisher"));
                 book.setYear(rs.getInt("year"));
-                book.setEdition_number(rs.getInt("edition_number"));
-                book.setBest_seller(rs.getInt("best_seller"));
+                book.setEditionNumber(rs.getInt("edition_number"));
+                book.setBestseller(rs.getInt("best_seller"));
             }
 
             rs.close();
@@ -95,10 +92,9 @@ public class bookDAOImpl implements bookDAO {
      * Retrieve all objects of books from database
      * table "books".
      * @return - list of book objects
-     * @throws SQLException
      */
     @Override
-    public List<Book> findAll() throws SQLException {
+    public List<Book> findAll() {
         ArrayList<Book> books = new ArrayList<>();
 
         try {
@@ -110,8 +106,8 @@ public class bookDAOImpl implements bookDAO {
                 book.setTitle(rs.getString("title"));
                 book.setPublisher(rs.getString("publisher"));
                 book.setYear(rs.getInt("year"));
-                book.setEdition_number(rs.getInt("edition_number"));
-                book.setBest_seller(rs.getInt("best_seller"));
+                book.setEditionNumber(rs.getInt("edition_number"));
+                book.setBestseller(rs.getInt("best_seller"));
                 books.add(book);
 
             }
@@ -127,18 +123,17 @@ public class bookDAOImpl implements bookDAO {
     /**
      * Add new object into the database table "books".
      * @param object - book object
-     * @throws SQLException
      */
     @Override
-    public void insert(Book object) throws SQLException {
+    public void insert(Book object) {
         try {
             PreparedStatement ps = db.con.prepareStatement(INSERT);
             ps.setString(1, object.getTitle());
             ps.setString(2, object.getPublisher());
             ps.setInt(3, object.getYear());
-            ps.setInt(4, object.getEdition_number());
-            ps.setInt(5, object.isBest_seller());
-            ps.setLong(6, object.getId_document());
+            ps.setInt(4, object.getEditionNumber());
+            ps.setInt(5, object.isBestseller());
+            ps.setLong(6, object.getDocumentId());
 
             ps.executeUpdate();
             ps.close();
@@ -151,18 +146,34 @@ public class bookDAOImpl implements bookDAO {
      * Update information about book which
      * is passing as argument.
      * @param object - book object
-     * @throws SQLException
      */
     @Override
-    public void update(Book object) throws SQLException {
+    public void update(Book object) {
         try{
             PreparedStatement ps = db.con.prepareStatement(UPDATE);
             ps.setString(1, object.getTitle());
             ps.setString(2, object.getPublisher());
             ps.setInt(3, object.getYear());
-            ps.setInt(4, object.getEdition_number());
-            ps.setInt(5, object.isBest_seller());
+            ps.setInt(4, object.getEditionNumber());
+            ps.setInt(5, object.isBestseller());
             ps.setLong(6, object.getId());
+
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateByIdDocument(Book object) {
+        try{
+            PreparedStatement ps = db.con.prepareStatement(UPDATE_BY_ID_DOCUMENT);
+            ps.setString(1, object.getTitle());
+            ps.setString(2, object.getPublisher());
+            ps.setInt(3, object.getYear());
+            ps.setInt(4, object.getEditionNumber());
+            ps.setInt(5, object.isBestseller());
+            ps.setLong(6, object.getDocumentId());
 
             ps.executeUpdate();
             ps.close();
@@ -175,30 +186,13 @@ public class bookDAOImpl implements bookDAO {
      * Delete all information about book from
      * the database table "books".
      * @param id - book id
-     * @throws SQLException
      */
     @Override
-    public void delete(long id) throws SQLException {
-        try {
-            PreparedStatement ps = db.con.prepareStatement(DELETE);
-            ps.setLong(1, id);
-
-            ps.executeUpdate();
-            ps.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void delete(long id) {
+        commonDAO.delete(id, "books", "id");
     }
 
-    public void deleteByDocumentId(long id) throws SQLException {
-        try {
-            PreparedStatement ps = db.con.prepareStatement(DELETE_BY_ID_DOCUMENT);
-            ps.setLong(1, id);
-
-            ps.executeUpdate();
-            ps.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void deleteByDocumentId(long id) {
+        commonDAO.delete(id, "books", "document_id");
     }
 }
