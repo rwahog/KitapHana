0a8ea02ee3ddb79d8dd253892eebf70d2754909c
rwahog <rwahog@gmail.com>
2018-04-09 11:26:47 +0300

    Update LibrarianPanelServlet.java

--- src/main/java/com/kitaphana/Servlet/LibrarianPanelServlet.java
+++ src/main/java/com/kitaphana/Servlet/LibrarianPanelServlet.java
@@ -3,8 +3,9 @@ package com.kitaphana.Servlet;
 import com.kitaphana.Entities.Document;
 import com.kitaphana.Entities.User;
 import com.kitaphana.Service.DocumentService;
-import com.kitaphana.Service.LibrarianPanelService;
+import com.kitaphana.Service.LibrarianService;
 import com.kitaphana.Service.LoginService;
+import com.kitaphana.Service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,36 +19,71 @@ import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/librarianPanel")
 public class LibrarianPanelServlet extends HttpServlet {
-    LibrarianPanelService service = new LibrarianPanelService();
+    LibrarianService service = new LibrarianService();
     DocumentService documentService = new DocumentService();
+    UserService userService = new UserService();
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession session = request.getSession();
-        ArrayList<User> arr = service.setUsersInfo();
-        ArrayList<Document> arr2 = service.setDocsInfo();
+        ArrayList<User> users = userService.findAll();
+        ArrayList<Document> docs = documentService.findAll();
         ArrayList<User> arr3 = service.setCheckoutsInfo();
         ArrayList<User> arr4 = service.setRenewsInfo();
         ArrayList<User> arr5 = service.setReturnsInfo();
         ArrayList<Document> arr6 = service.setWaintingsInfo();
-        session.setAttribute("docs", arr2);
-        session.setAttribute("users", arr);
-        request.setAttribute("users_checkout", arr3);
+        session.setAttribute("docs", docs);
+        session.setAttribute("users", users);
+        request.setAttribute("usersCheckouts", arr3);
         request.setAttribute("renews", arr4);
         request.setAttribute("returns", arr5);
-        request.setAttribute("waiting_list", arr6);
+        request.setAttribute("waitingList", arr6);
         try {
             new LoginService().redirect(request, response, "librarianPanel", true);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-//        String approval_checkout = request.getParameter("button");
-
-            documentService.checkout_approval(request.getParameter("id_user"), request.getParameter("id_doc"));
-            response.sendRedirect("/main");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        if (request.getParameter("checkout_approval") != null && Integer.parseInt(request.getParameter("checkout_approval")) == 1) {
+            documentService.checkOutApproval(request.getParameter("user_id"), request.getParameter("doc_id"));
+            try {
+                documentService.sendMsg(documentService.getChatId(Long.parseLong(request.getParameter("user_id"))), "You have checked out new book successfully!");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            response.sendRedirect("/main");
+        } else if (request.getParameter("checkout_disapproval") != null && Integer.parseInt(request.getParameter("checkout_disapproval")) == 1) {
+            documentService.checkOutDisapproval(request.getParameter("user_id"), request.getParameter("doc_id"));
+            try {
+                documentService.sendMsg(documentService.getChatId(Long.parseLong(request.getParameter("user_id"))), "You have checked out new book unsuccessfully :(");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            response.sendRedirect("/main");
+        } else if (request.getParameter("renew_approval") != null && Integer.parseInt(request.getParameter("renew_approval")) == 1) {
+            documentService.renewDocApproval(request.getParameter("user_id"), request.getParameter("doc_id"));
+            try {
+                documentService.sendMsg(documentService.getChatId(Long.parseLong(request.getParameter("user_id"))), "Renew was approved :)");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            response.sendRedirect("/main");
+        } else if ((request.getParameter("renew_disapproval") != null && Integer.parseInt(request.getParameter("renew_disapproval")) == 1)){
+            documentService.renewDocDisApproval(request.getParameter("user_id"), request.getParameter("doc_id"));
+            try {
+                documentService.sendMsg(documentService.getChatId(Long.parseLong(request.getParameter("user_id"))), "Renew wasn't approved :(");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            response.sendRedirect("/main");
+        } else if ((request.getParameter("return_approval") != null && Integer.parseInt(request.getParameter("return_approval")) == 1)) {
+            documentService.returnDocApproval(request.getParameter("user_id"), request.getParameter("doc_id"));
+            response.sendRedirect("/main");
+        }
     }
 }
