9daa4fe5fa3ddb90e8a62476d27726d23b138599
rwahog <rwahog@gmail.com>
2018-04-09 11:25:59 +0300

    Update DocumentServlet.java

--- src/main/java/com/kitaphana/Servlet/DocumentServlet.java
+++ src/main/java/com/kitaphana/Servlet/DocumentServlet.java
@@ -2,6 +2,7 @@ package com.kitaphana.Servlet;
 
 import com.kitaphana.Entities.Document;
 import com.kitaphana.Service.DocumentService;
+import com.kitaphana.Entities.Book;
 import com.kitaphana.Service.LoginService;
 
 import javax.servlet.ServletException;
@@ -9,39 +10,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/document")
 public class DocumentServlet extends HttpServlet {
     boolean checkOut;
     long id_user;
     int id;
-    long chat_id;
     private DocumentService service = new DocumentService();
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         Document document = service.setDocInfo(request.getParameter("id"));
         request.setAttribute("document", document);
+        request.setAttribute("doc_button_available", "true");
+
         try {
             new LoginService().redirect(request, response, "document", false);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        //
-        // request.getRequestDispatcher("/WEB-INF/views/document.jsp").forward(request, response);
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         id_user = Long.parseLong(String.valueOf(request.getSession().getAttribute("id")));
         id = Integer.parseInt(request.getParameter("id"));
-        chat_id = Long.parseLong(String.valueOf(request.getSession().getAttribute("chat_id")));
-        checkOut = service.checkOut(id_user, id);
-        if (checkOut) {
-            response.sendRedirect("/verification");
-            try {
-                service.sendMsg(service.getChatId(id), "You have checked out new book successfully!");
+        String checkout_button = request.getParameter("checkout");
+        if (checkout_button.equals("checkout")) {
+            checkOut = service.checkOut(id_user, id);
+            if (checkOut) {
+                response.sendRedirect("/verification");
             }
-            catch (Exception e){
+        } else if (checkout_button.equals("queue")) {
+            try {
+                checkOut = service.queue(id_user, id);
+                if (checkOut) {
+                    response.sendRedirect("/verification");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
