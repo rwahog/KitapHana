4a832c9670897ac28f7f48daaca7abdbf8daef7f
rwahog <arcticboyinblue@gmail.com>
2018-04-09 11:56:45 +0300

    fix

--- src/main/java/com/kitaphana/Servlet/ProfileServlet.java
+++ src/main/java/com/kitaphana/Servlet/ProfileServlet.java
@@ -1,9 +1,9 @@
 package com.kitaphana.Servlet;
 
+import com.kitaphana.Entities.Address;
 import com.kitaphana.Entities.User;
-import com.kitaphana.Service.EditUserService;
 import com.kitaphana.Service.LoginService;
-import com.kitaphana.Service.ProfileService;
+import com.kitaphana.Service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,15 +16,14 @@ import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/profile")
 public class ProfileServlet extends HttpServlet {
-    EditUserService serviceEdit = new EditUserService();
-    ProfileService service = new ProfileService();
+    UserService userService = new UserService();
     User user = new User();
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession session = request.getSession();
         if (session.getAttribute("id") != null) {
-            user = service.setUserInfo(session.getAttribute("id").toString());
+            user = userService.findUserById(Long.parseLong(session.getAttribute("id").toString()));
         }
         session.setAttribute("user", user);
         try {
@@ -46,18 +45,17 @@ public class ProfileServlet extends HttpServlet {
         String country = request.getParameter("country");
         String town = request.getParameter("town");
         String street = request.getParameter("street");
-        String house_number = request.getParameter("house_number");
-        String apartment_number = request.getParameter("apartment_number");
+        int house_number = Integer.parseInt(request.getParameter("house_number"));
+        int apartment_number = Integer.parseInt(request.getParameter("apartment_number"));
         String post_code = request.getParameter("postcode");
-        String chat_id = request.getParameter("chat_id");
 
-        boolean isValid = serviceEdit.isValid(Integer.parseInt(request.getParameter("id")), phone_number, password1, password2);
+        boolean isValid = userService.isValid(Integer.parseInt(request.getParameter("id")), phone_number, password1, password2);
         if (isValid) {
-            try {
-                serviceEdit.editUser(name, surname, status, phone_number, password1, email, country, town, street, house_number, apartment_number, post_code, Integer.parseInt(request.getParameter("id")), user.getAddress().getId_address());
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            Address address = new Address(country, town, street, house_number, apartment_number, post_code);
+            address.setAddressId(userService.getUserAddressId(Long.parseLong(request.getParameter("id"))));
+            User user = new User(name, surname, phone_number, password1, email, address, status);
+            user.setId(Long.parseLong(request.getParameter("id")));
+            userService.editUserInfo(user);
             response.sendRedirect("/main");
         } else {
             request.getRequestDispatcher("WEB-INF/views/profile.jsp").forward(request, response);
