61bdd73190a55fd6d0a4d680d2b9c6d551b9de30
Almir Mullanurov <35490819+Mirlan-code@users.noreply.github.com>
2018-04-10 23:45:39 +0300

    Add files via upload
    search

---
+++ src/main/java/com/kitaphana/Service/Trie.java
@@ -0,0 +1,94 @@
+import javafx.collections.transformation.SortedList;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Trie {
+    protected Node root;
+    protected int size = 10;
+    protected Connection connection;
+    protected Statement statement;
+    protected HashMap<String, Integer> priority;
+    Trie(Connection connection) throws SQLException {
+        this.connection = connection;
+        this.statement = connection.createStatement();
+        initializeTrie();
+        priority = new HashMap<>();
+    }
+    class Node
+    {
+        HashMap<Character, Node> next = new HashMap<Character, Node>();
+        ArrayList<String> possible_strings = new ArrayList<String>();
+        boolean isEndOfWord;
+        Node(){
+            isEndOfWord = false;
+        }
+    };
+    public void initializeTrie() throws SQLException {
+        ResultSet resultSet = statement.executeQuery("select * from search");
+        while(resultSet.next()){
+            String query = resultSet.getString("query");
+            int amount = resultSet.getInt("amount");
+            priority.put(query, amount);
+            add(query);
+        }
+    }
+
+    public void add(String s){
+        Node curNode = root;
+        for(int i = 0; i<s.length(); i++){
+            if(curNode.possible_strings.size()<size){
+                curNode.possible_strings.add(s);
+            }
+            else if(curNode.possible_strings.size()==size && priority.get(curNode.possible_strings.get(size-1)) < priority.get(s)){
+                curNode.possible_strings.set(size-1, s);
+            }
+            Collections.sort(curNode.possible_strings);
+            Character next = s.charAt(i);
+            if(curNode.next.containsKey(next)){
+                curNode = curNode.next.get(next);
+            }
+            else{
+                Node newNode = new Node();
+                curNode.next.put(next, newNode);
+                curNode = newNode;
+            }
+        }
+        curNode.possible_strings.add(s);
+        curNode.isEndOfWord = true;
+    }
+    public Node find(String s){
+        Node curNode = root;
+        for(int i = 0; i<s.length(); i++){
+            Character next = s.charAt(i);
+            if(!curNode.next.containsKey(next)){
+                return null;
+            }
+            else{
+                curNode = curNode.next.get(next);
+            }
+        }
+        return curNode;
+    }
+    public ArrayList<String> get(String s){
+        Node curNode = root;
+        for(int i = 0; i<s.length(); i++){
+            Character next = s.charAt(i);
+            if(!curNode.next.containsKey(next)){
+                return curNode.possible_strings;//Если впереди идут ошибки, то возврщаем последние возможные слова
+            }
+            else{
+                curNode = curNode.next.get(next);
+            }
+        }
+        return curNode.possible_strings;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+}
