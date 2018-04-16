b3a70da6a88fa040c0a156170c11e17f70e490eb
rwahog <rwahog@gmail.com>
2018-04-16 20:33:10 +0300

    Create AuthorizationFilter.java

---
+++ src/main/java/com/kitaphana/Servlet/Filters/AuthorizationFilter.java
@@ -0,0 +1,37 @@
+package com.kitaphana.Servlet.Filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/AuthorizationFilter")
+public class AuthorizationFilter implements Filter {
+    private ServletContext context;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.context = filterConfig.getServletContext();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+        if (session.getAttribute("librarian") == null) {
+            response.sendRedirect("/main");
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
